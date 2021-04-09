f70c50d83568062267e254612f05a87e50a9f0e0
Sourabh Sharma <sourabh@uplad.in>
2021-04-09 18:39:47 +0530

    Added bar graphs for Top 6 Credit and Debit.

--- visualize.py
+++ visualize.py
@@ -6,9 +6,8 @@ from pandasql import sqldf
 sql=lambda q: sqldf(q,globals())
 import re
 
-
+#Pie Chart Visualization of Payment Area
 df = pd.read_csv("yes.csv")
-print(df.head())
 df_pay_area = sql("""
 SELECT `Payment_Area`,COUNT(*) as Total
 FROM df
@@ -17,7 +16,7 @@ order by Total desc
 limit 6
 """)
 
-df_pay_area.set_index("Payment_Area",inplace=True)
+df_pay_area.set_index("Payment_Area")
 colors_list = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue', 'lightgreen', 'pink']
 explode_list = [0.1, 0,0.1, 0,0.1, 0] # ratio for each continent with which to offset each wedge.
 
@@ -36,9 +35,45 @@ df_pay_area['Total'].plot(kind='pie',
 
 # scale the title up by 12% to match pctdistance
 plt.title('Distribution by Payment Area', y=1.05)
-
 plt.axis('equal')
-
 # add legend
 plt.legend(labels=df_pay_area.index, loc='upper left')
-plt.savefig("yes_pay_area_pie.png")
+plt.savefig("pay_area.png")
+
+
+
+#Bar graph for Top 6 Debits
+df_from_debit = sql("""
+SELECT `From`,SUM(`Debit`) as Total
+FROM df
+group by `From`
+order by Total desc
+limit 6
+""")
+
+df_from_debit.set_index("From",inplace=True)
+df_from_debit.plot(kind='bar')
+
+plt.rcParams.update({'font.size': 17})
+plt.ylabel("Total Amount")
+plt.xlabel("Entity")
+plt.savefig("Debit.png",bbox_inches = 'tight')
+
+
+
+#Bar graph for Top 6 Credits
+df_from_credit = sql("""
+SELECT `From`,SUM(`Credit`) as Total
+FROM df
+group by `From`
+order by Total desc
+limit 6
+""")
+
+df_from_credit.set_index("From",inplace=True)
+df_from_credit.plot(kind='bar')
+
+plt.rcParams.update({'font.size': 17})
+plt.ylabel("Total Amount")
+plt.xlabel("Entity")
+plt.savefig("Credit.png",bbox_inches = 'tight')
